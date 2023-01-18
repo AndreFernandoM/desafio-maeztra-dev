9bb691ae7a3d59e1f1d497c5ec5aa9e1f446368c
AndreFernandoM <andrefernando777@hotmail.com>
2023-01-18 17:15:14 -0300

    feat: asw 1

--- resposta_questao_1.js
+++ resposta_questao_1.js
@@ -1,1 +1,22 @@
+function OrderCheck(input) {
+    var numArr = [...input.toString()].map(Number);
+    var crescente = true;
+    var decrescente = true;
+    for (var i = 0; i < numArr.length - 1; i++) {
+        if (numArr[i] + 1 !== numArr[i + 1] && numArr[i] !== numArr[i + 1]) {
+            crescente = false;
+        }
+        if (numArr[i] - 1 !== numArr[i + 1] && numArr[i] !== numArr[i + 1]) {
+            decrescente = false;
+        }
+    }
+    if (crescente || decrescente) {
+        return "Está ordenado";
+    }else {
+        return "Não está ordenado";
+    }
+}
 
+
+
+console.log(OrderCheck(1599));
