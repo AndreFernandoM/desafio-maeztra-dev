726ee5ba21deb5d652609bca40ce2380d8a0e5f2
AndreFernandoM <andrefernando777@hotmail.com>
2023-01-19 09:25:16 -0300

    fix: remove clg

--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -1,1 +1,13 @@
+function tupleMake(input){
+    let newTuple = []
+    for(let i=0;i<input.length;i++){
+        for(let j=i+1;j<input.length;j++){
+            if(input[i] === input[j]){
+                newTuple[newTuple.length]=input[i]
+            }
+        }
+    }
+        return newTuple;
+}
 
+console.log(tupleMake([4,5,44,98,4,5,6,6]))
