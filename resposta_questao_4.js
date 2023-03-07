fc3925258b77d9f1ee845347c2ab66ff534f8e32
AndreFernandoM <andrefernando777@hotmail.com>
2023-03-06 23:31:21 -0300

    feat: aswr 4

--- resposta_questao_4.js
+++ resposta_questao_4.js
@@ -1,1 +1,23 @@
+const matrix = [
+  [1960, 2005],
+  [1945, 2008],
+  [1938, 1999]
+];
 
+function findYearsBetween(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+}
+
+function findMostWorkedYears(matrix) {
+  let startedWorking = 0;
+  let retirementYear = Number.MAX_SAFE_INTEGER;
+
+  matrix.forEach((element) => {
+    startedWorking = startedWorking < element[0] ? element[0] : startedWorking;
+    retirementYear = retirementYear > element[1] ? element[1] : retirementYear;
+  });
+
+  return findYearsBetween(startedWorking, retirementYear);
+}
+
+console.log(findMostWorkedYears(matrix));
